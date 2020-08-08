71e93656d0ad14abf9e828f100751969a674502f
smitteh1 <thejamesash1@gmail.com>
2020-08-08 16:00:05 -0400

    add Vector3 class

---
+++ src/com/jamesql/Engine/math/Vector3.java
@@ -0,0 +1,21 @@
+package com.jamesql.Engine.math;
+
+public class Vector3<T> {
+
+	T x;
+	T y;
+	T z;
+	
+	public Vector3(T x, T y, T z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	
+	// dont need to implement rn
+	public void add()
+	{
+		
+	}
+	
+}
