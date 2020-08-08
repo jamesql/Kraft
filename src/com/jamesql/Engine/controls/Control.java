79598b0fe3fe4c9e345baa213aee15c288c9ea5c
smitteh1 <thejamesash1@gmail.com>
2020-08-08 14:47:38 -0400

    rework how controls work again

--- src/com/jamesql/Engine/controls/Control.java
+++ src/com/jamesql/Engine/controls/Control.java
@@ -4,6 +4,12 @@ import java.util.ArrayList;
 
 public abstract class Control { 
 	
+	private Keycode kc;
+	
+	Control(int c) { kc=new Keycode(c); }
+	
 	public abstract void run(ArrayList<Object> args); 
+	
+	public Keycode getCode() { return kc; }
 	
 }
--- src/com/jamesql/Engine/controls/TestControl.java
+++ src/com/jamesql/Engine/controls/TestControl.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 
 public class TestControl extends Control {
 	
+	// Enter keycode here
+	TestControl() { super(0); };
+	
 	@Override
 	public void run(ArrayList<Object> args) {
 		System.out.println(args);
